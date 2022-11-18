5a5c70b6713bfe662ebc603f275bb7e415dcf5c2
Robert <soarerobert17@gmail.com>
2022-11-18 11:09:57 +0200

    Fix fragment

--- shaders/simple_fragment.glsl
+++ shaders/simple_fragment.glsl
@@ -1,20 +1,13 @@
 #version 330 core
-layout (location = 0) in vec3 pos;
-layout (location = 1) in vec4 color;
-layout (location = 2) in vec3 normal;
-layout (location = 3) in vec2 texCoord;
 
-out vec4 Color;
-out vec2 TexCoord;
+in vec4 Color;
+in vec2 TexCoord;
 
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
+out vec4 FragColor;
 
+uniform sampler2D texture0;
 
 void main()
 {
-  gl_Position = projection * view * model * vec4(pos.x, pos.y, pos.z, 1.0);
-  Color = color;
-  TexCoord = texCoord;
+  FragColor = texture(texture0, TexCoord);
 }
